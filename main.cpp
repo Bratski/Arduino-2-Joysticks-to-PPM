aa53538bb559e5815223ddaa6f573da4cbedaf90
Bart <89835964+Bratski@users.noreply.github.com>
2023-05-19 18:21:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,14 @@
 #include <usbhub.h> 
 #include "PPMEncoder.h"
 
-#define OUTPUT_PIN 3
+#define OUTPUT_PIN 3 // define the Arduino output pin for the PPM output signal
 
 unsigned int debug = 1; // turn serial printing on (1) or off (0) for debugging
 
 // Logitech Attack 3 HID report 
 struct GamePadEventData { 
-	uint8_t X, Y;
-	uint8_t Z1, Z2, Rz; 
-} __attribute__((packed)); 
+	uint8_t X, Y, Z1, Z2, Rz; 
+};
 
 class JoystickEvents { 
 	public: virtual void OnGamePadChanged(const GamePadEventData *evt, int joystick); 
@@ -37,36 +36,37 @@ void JoystickReportParser::Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8
 } 
  
 void JoystickEvents::OnGamePadChanged(const GamePadEventData *evt, int joystick) { 
-	// Serial.print("Joystick "); 
-	// Serial.print(joystick); 
+	
 	if (joystick == 1) {
 		if (debug == 1){
-			Serial.print(" J1 X raw: "); Serial.print(evt->X);
-			Serial.print(" J1 Y raw: "); Serial.print(evt->Y);
-			Serial.print(" J1 Z1 raw: "); Serial.print(evt->Z1); 
-			Serial.print(" J1 Z2 raw: "); Serial.print(evt->Z2); 
-			Serial.print(" J1 Rz raw: "); Serial.print(evt->Rz); 
+			Serial.print(" Joystick: "); Serial.print(joystick);
+			Serial.print(" X : "); Serial.print(evt->X);
+			Serial.print(" Y : "); Serial.print(evt->Y);
+			Serial.print(" Z1 : "); Serial.print(evt->Z1); 
+			Serial.print(" Z2 : "); Serial.print(evt->Z2); 
+			Serial.print(" Rz : "); Serial.print(evt->Rz); 
 			Serial.println();
 		} 
-		ppmEncoder.setChannelPercent(0, map(evt->X, 0, 255, 0, 50));
-		ppmEncoder.setChannelPercent(1, map(evt->Y, 0, 255, 0, 50));
-		ppmEncoder.setChannelPercent(2, map(evt->Z1, 0, 255, 0, 50));
-		ppmEncoder.setChannelPercent(3, map(evt->Z2, 0, 255, 0, 50)); // buttons
+		ppmEncoder.setChannelPercent(0, map(evt->X, 0, 255, 0, 50)); // X-Axis on PPM channel 1
+		ppmEncoder.setChannelPercent(1, map(evt->Y, 0, 255, 0, 50)); // Y-Axis on PPM channel 2
+		ppmEncoder.setChannelPercent(2, map(evt->Z1, 0, 255, 0, 50)); // Z-Potentiometer on PPM channel 3
+		ppmEncoder.setChannelPercent(3, map(evt->Z2, 0, 255, 0, 50)); // buttons on PPM channel 4
 	}
 	else if (joystick == 2) {
 		if (debug == 1){
-			Serial.print(" J2 X raw: "); Serial.print(evt->X);
-			Serial.print(" J2 Y raw: "); Serial.print(evt->Y);
-			Serial.print(" J2 Z1 raw: "); Serial.print(evt->Z1); 
-			Serial.print(" J2 Z2 raw: "); Serial.print(evt->Z2); 
-			Serial.print(" J2 Rz raw: "); Serial.print(evt->Rz); 
+			Serial.print(" Joystick: "); Serial.print(joystick);
+			Serial.print(" X : "); Serial.print(evt->X);
+			Serial.print(" Y : "); Serial.print(evt->Y);
+			Serial.print(" Z1 : "); Serial.print(evt->Z1); 
+			Serial.print(" Z2 : "); Serial.print(evt->Z2); 
+			Serial.print(" Rz : "); Serial.print(evt->Rz); 
 			Serial.println();
 		}
 
-		ppmEncoder.setChannelPercent(4, map(evt->X, 0, 255, 0, 50));
-		ppmEncoder.setChannelPercent(5, map(evt->Y, 0, 255, 0, 50));
-		ppmEncoder.setChannelPercent(6, map(evt->Z1, 0, 255, 0, 50));
-		ppmEncoder.setChannelPercent(7, map(evt->Z2, 0, 255, 0, 50)); // buttons 
+		ppmEncoder.setChannelPercent(4, map(evt->X, 0, 255, 0, 50)); // X-Axis on PPM channel 5
+		ppmEncoder.setChannelPercent(5, map(evt->Y, 0, 255, 0, 50)); // Y-Axis on PPM channel 6
+		ppmEncoder.setChannelPercent(6, map(evt->Z1, 0, 255, 0, 50)); // Z-Potentionmeter on PPM channel 7
+		ppmEncoder.setChannelPercent(7, map(evt->Z2, 0, 255, 0, 50)); // buttons on PPM channel 8
 	}
 } 
 
@@ -81,9 +81,7 @@ JoystickReportParser Joy2(&JoyEvents, 2);
 
 void setup() { 
 	Serial.begin(115200); 
-	// #if !defined(__MIPSEL__) while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection 
-	// #endif 
-	
+		
 	Serial.println("Start"); 
 	
 	ppmEncoder.begin(OUTPUT_PIN);
